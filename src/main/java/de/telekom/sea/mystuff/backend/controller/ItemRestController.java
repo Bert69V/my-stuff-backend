2b5444374c02d260d69f7c92ace3852f764f4e9c
Bert Voitl <bert.voitl@gmx.de>
2020-06-10 17:11:49 +0200

    controller geändert plus test

--- src/main/java/de/telekom/sea/mystuff/backend/controller/ItemRestController.java
+++ src/main/java/de/telekom/sea/mystuff/backend/controller/ItemRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import de.telekom.sea.mystuff.backend.entity.Item;
 import de.telekom.sea.mystuff.backend.repository.ItemRepo;;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/v1/items")	
 public class ItemRestController {
 
